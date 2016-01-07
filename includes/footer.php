82459ccd7adf32cfc1bcaa2eda54534d1cbcc658
Yohei Kanehara <kanehara@roosta.dreamhost.com>
2016-01-06 21:33:41 -0800

    Added animations of nav bar

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,5 @@
 <footer>
     <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-    <script type="text/javascript" src="js/jquery.smooth-scroll.js"></script>
-    <script>
-            $('.smooth').on('click', function() {
-                        $.smoothScroll({
-                                        scrollElement: $('body'),
-                    scrollTarget: '#' + this.id
-                        });
-                            
-                            return false;
-            });
-    </script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
--- js/script.js
+++ js/script.js
@@ -26,24 +26,54 @@ function applyCoverListener() {
 /*
 *   NAV BAR SCROLL SCRIPTS
 */
-function triggerShowNav() {
-    
-}
-
-function triggerHideNav() {
-
+// controls the revealing of nav bar when scrolling from home screen
+function revealNav() {
+    var finalScrollPos = $('.cover').height()*.5 // the scroll position when nav is fully displayed
+    var percentScrolled = $(window).scrollTop()/finalScrollPos;
+    percentScrolled = percentScrolled >= 1 ? 1 : percentScrolled;
+    if ($(window).width() > 800) {
+        var minLeft = -90;
+        var animationLen = 115; // the ammount of pixels we're animating
+        var curLeft = minLeft + animationLen*percentScrolled;
+        $('.masthead').css({"left":curLeft + 'px', "top": 0});
+    }
+    else { // for when nav bar is at top
+        var minTop = -60;
+        var animationLen = 60;
+        var curTop = minTop + animationLen*percentScrolled;
+        $('.masthead').css({"top": curTop + 'px', "left": 0});
+    }
 }
 
 function applyNavListener() {
     $(window).scroll(function() {
         if($(window).scrollTop() > $('.cover').height()) {
             $('.masthead').addClass('masthead-fixed');
-            triggerShowNav();
         }
         if ($(window).scrollTop() < $('.cover').height()) {
             $('.masthead').removeClass('masthead-fixed');
-            triggerHideNav();
+            revealNav();
         }
+    });
+    $(window).on('resize', function() {
+        revealNav();
     });
 }
 
+/*
+*   SMOOTH SCROLL
+*/
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 600);
+                return false;
+            }
+        }
+    });
+});
