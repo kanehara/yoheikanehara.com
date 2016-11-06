1b3fceb59d39b1d7a336e5385e10b128c31115b7
yohei.kanehara <yohei.kanehara@vodori.com>
2016-11-05 22:59:19 -0500

    feature/LESS - Configured Grunt and converted CSS files to LESS

---
+++ grunt/config/watch.js
@@ -0,0 +1,9 @@
+module.exports = {
+    styles: {
+        files: ['styling/less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+            nospawn: true
+        }
+    }
+}
